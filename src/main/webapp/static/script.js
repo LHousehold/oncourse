a544ebb5094468683919cd2e6feb5e0510540ae0
LHousehold <luke.household@outlook.com>
2015-11-14 12:38:49 -0500

    Removed mockups folder

---
+++ src/main/webapp/static/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+  $('.navbar a').click(function(e){
+    $('.navbar li.active').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+      $this.parent().addClass('active');
+    }
+    e.preventDefault();
+    return false;
+  });
+
+  $('.sidebar_top a').click(function(e){
+    $('.sidebar_top a.active').removeClass('active');
+    var $this = $(this);
+    $this.addClass('active');
+    e.preventDefault();
+    return false;
+  });
+
+  $('.navbar-brand').click(function(e){
+    var $this = $(this);
+    
+    var $navbar = $this.parents(".navbar");
+    var $active = $navbar.find(".active");
+    var $default_active = $navbar.find(".default_active");
+
+    $active.removeClass('active');
+    $default_active.addClass('active');
+
+    e.preventDefault();
+    return false;
+  });
+
+  $('.course_menu_item').click(function(e){
+    var $this = $(this);
+
+    var course_code = $this.attr("data-course-code");
+  });
+
+})
