b0bfd93f40f2c6e0510450550bc33f651e566534
Luke Household <luke@localhost.localdomain>
2015-11-17 15:07:40 -0500

    Added left and right buttons to course package reader

--- src/main/webapp/static/cp_script.js
+++ src/main/webapp/static/cp_script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     // All the Javascript that coursepackage.xhtml will need
 
+    var load_page = function(course_code, page_number){
+        $("#page_content").load("cppage.xhtml", {"coursecode": course_code, "pagenumber": page_number});
+    };
+
     $('#page_content').ready(function(e){
         // When course package loads, load first page
         var course_code = $(".coursepackage_title").attr("data-course-code");
@@ -9,8 +13,36 @@ $(document).ready(function() {
         load_page(course_code,page_number);
     });
 
-    var load_page = function(course_code, page_number){
-        $("#page_content").load("cppage.xhtml", {"coursecode": course_code, "pagenumber": page_number});
-    };
+    $('.left_arrow').click(function(e){
+        // go get current page number and course code
+        var page_number = $(".cppage_top").attr("data-page-number");
+        var course_code = $(".coursepackage_title").attr("data-course-code");
+
+        var page_number = parseInt(page_number,10) - 1;
+
+        if (page_number < 1) {
+            // if on page 1, do nothing
+            return;
+        }
+
+        load_page(course_code,page_number);
+    });
+
+    $('.right_arrow').click(function(e){
+        // go get current page number and course code
+        var page_number = $(".cppage_top").attr("data-page-number");
+        var course_code = $(".coursepackage_title").attr("data-course-code");
+
+        var page_number = parseInt(page_number,10) + 1;
+
+        var max_pages = 100; // actual number will be stored
+
+        if (page_number > max_pages) {
+            // if on last page, do nothing
+            return;
+        }
+
+        load_page(course_code,page_number);
+    });
 
 })
