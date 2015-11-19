b8aa6a1cf04bbf0d694c81ee94a9fd3e2db62366
Chris von Zuben <chris.vonzuben@gmail.com>
2015-11-19 18:46:12 -0500

    connecting to the database works now

--- src/main/java/com/oncourse/DbConnector.java
+++ src/main/java/com/oncourse/DbConnector.java
@@ -1,4 +1,4 @@
-package oncourse;
+package com.oncourse;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -11,14 +11,28 @@ import java.sql.SQLException;
 public class DbConnector {
 
     public void go() {
-        String url1 = "jdbc:mysql://localhost:3306/oncourse";
+        String url = "jdbc:mariadb://localhost:3306/oncourse";
         String user = "oc";
         String password = "";
         Connection conn;
 
         try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
 
-            conn = DriverManager.getConnection(url1, user, password);
+            //Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("YOYOYO");
+            Class.forName("org.mariadb.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+        }
+
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mariadb://localhost/oncourse",
+                    "oc",
+                    "");
+            //conn = DriverManager.getConnection(url, user, password);
             if (conn != null) {
                 System.out.println("Connected to the database test1");
             }
