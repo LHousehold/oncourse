a5bee18e4745472adb02a12bbff6a364e70ad880
Chris von Zuben <chris.vonzuben@gmail.com>
2015-11-22 23:44:34 -0500

    design for databse api finished, still needs each table class

--- src/main/java/com/oncourse/DbConnector.java
+++ src/main/java/com/oncourse/DbConnector.java
@@ -2,12 +2,18 @@ package com.oncourse;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ApplicationScoped;
+
 import javax.annotation.PreDestroy;
-//import javax.faces.bean.ViewScoped;
+
+import java.lang.reflect.InvocationTargetException;
+
+import java.util.List;
+import java.util.ArrayList;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.sql.*;
 
 //@ManagedBean(name = "coursePackageDisplay", eager = true)
 @ManagedBean(name = "dbConnection", eager = true)
@@ -21,10 +27,12 @@ public class DbConnector {
         open();
     }
 
+    // not sure this actually works
     @PreDestroy
     public void closeConn() {
         try {
             conn.close();
+            System.out.println("Connected to the database oncourse");
         }
         catch (SQLException ex) {
             System.out.println("An error occurred. Maybe user/password is invalid");
@@ -36,14 +44,70 @@ public class DbConnector {
         return this.conn;
     }
 
+    // entry should be a new instance of the type of table you are reading from
+    public <T extends DbTable> DbTable readTable(DbTable entry, Class<T> type) {
+        //String query = "SELECT * FROM course_package WHERE cpid = " + cpid + " AND page = " + page;
+
+        // when this is created, the first entry willl just act as a head and have no valuse by it self
+        // call next to get the first entry with values
+        //T entry;
+        String query;
+        System.out.println("begin");
+
+        query  = entry.readQuery();
+
+//        try {
+//            entry = type.newInstance();
+//            query = entry.readQuery();
+//        } catch (InstantiationException x) {
+//            x.printStackTrace();
+//            return null;
+//        } catch (IllegalAccessException x) {
+//            x.printStackTrace();
+//            return null;
+//        }
+
+        System.out.println("start to read from table");
+        try {
+            // create the java statement
+            Statement st = conn.createStatement();
+
+            // execute the query, and get a java resultset
+            ResultSet rs = st.executeQuery(query);
+
+            // iterate through the java resultset
+            while (rs.next()) {
+                // create this to insert it in to the list of results from the query
+                T newEntry;
+                try {
+                    newEntry = type.newInstance();
+                    newEntry.fill(rs);
+                    entry.push(newEntry);
+                } catch (InstantiationException x) {
+                    x.printStackTrace();
+                    return null;
+                } catch (IllegalAccessException x) {
+                    x.printStackTrace();
+                    return null;
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Error reading the Database");
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println("finied");
+        return entry;
+
+    }
+
     public String tst() {
         return yay;
     }
 
     private void open() {
-        String url = "jdbc:mariadb://localhost:3306/oncourse";
-        String user = "oc";
-        String password = "";
 
         try {
             // The newInstance() call is a work around for some
