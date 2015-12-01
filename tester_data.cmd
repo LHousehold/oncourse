97eb1ee961242491a6f844f13a7ab109c9bc453d
Howard Zhang <howeey@localhost.localdomain>
2015-12-01 00:54:40 -0500

    some tester data for the database. Just run tester.sh

---
+++ tester_data.cmd
@@ -0,0 +1,14 @@
+
+use oncourse;
+
+INSERT INTO course_package
+VALUES (NULL,1,"YOUTUBE", 1, "IdneKLhsWOQ", "TOP");
+
+INSERT INTO course_package
+VALUES (NULL,1,"YOUTUBE", 1, "lp-EO5I60KA", "BL");
+
+INSERT INTO course_package
+VALUES (NULL,1,"YOUTUBE", 1, "nSDgHBxUbVQ", "BR");
+
+INSERT INTO course_package
+VALUES (NULL,1,"YOUTUBE", 2, "dwTq_eblLyk", "FULL");
