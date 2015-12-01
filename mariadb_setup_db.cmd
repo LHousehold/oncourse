9f3d4e3a456179af2e3f12063d529b2ebc3cb4bb
Howard Zhang <howeey@localhost.localdomain>
2015-12-01 00:53:55 -0500

    changed initial setup script. Added Sections table and made IDs on tables auto increment

--- mariadb_setup_db.cmd
+++ mariadb_setup_db.cmd
@@ -5,44 +5,57 @@ use oncourse;
 
 CREATE TABLE IF NOT EXISTS course_package
 (
-id int NOT NULL,
+id int AUTO_INCREMENT,
 cpid int NOT NULL,
 media_type varchar(20) NOT NULL,
 page int NOT NULL,
 source varchar(255) NOT NULL,
-location varchar(10)
+location varchar(10),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS course_package_section
+(
+id int AUTO_INCREMENT,
+cpid int NOT NULL,
+section_name varchar(255) NOT NULL,
+page int NOT NULL,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-id int NOT NULL,
+id int AUTO_INCREMENT,
 user_name varchar(255) NOT NULL,
 email varchar(255) NOT NULL,
-type varchar(10) DEFAULT 'student'
+type varchar(10) DEFAULT 'student',
+PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS course_package_name
 (
-id int NOT NULL,
+id int AUTO_INCREMENT,
 name varchar(255) NOT NULL,
-course_code varchar(255)
+course_code varchar(255),
+PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS user_save
 (
-id int NOT NULL,
+id int AUTO_INCREMENT,
 uid int NOT NULL,
 cpid int NOT NULL,
-page int NOT NULL DEFAULT 1
+page int NOT NULL DEFAULT 1,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS user_permission
 (
-id int NOT NULL,
+id int AUTO_INCREMENT,
 uid int NOT NULL,
 cpid int NOT NULL,
-permission_level varchar(255) NOT NULL DEFAULT 'read'
+permission_level varchar(255) NOT NULL DEFAULT 'read',
+PRIMARY KEY (id)
 );
 
 GRANT ALL PRIVILEGES ON oncourse.* TO 'oc'@'localhost';
-
