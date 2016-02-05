0e18e70caf6453a6e365d635ddbf8a260ca99e21
Luke Household <luke.household@outlook.com>
2016-02-04 21:58:41 -0500

    Made indices auto-update

--- src/main/webapp/static/cpedit_script.js
+++ src/main/webapp/static/cpedit_script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // All the Javascript that coursepackageeditor.xhtml will need
 
-    $(".edit").focus(function(e){
+    $("#sections_content").on("focus", ".edit", function(e){
         setTimeout(function(){
             // ensure focus is complete
             // this is because with no delay whole page is selected
@@ -19,24 +19,15 @@ $(document).ready(function() {
         }, 20);
     });
 
-    $(".edit").keypress(function(e){
+    $("#sections_content").on("keypress", ".edit", function(e){
         if (e.which === 13) {
             e.preventDefault();
         }
     });
 
-    // $(".add_subsection").click(function(e){
-    //     var new_el = add_subsection($(this));
-    //     var add_subsection_btn = new_el.find(".add_subsection");
-    //     console.log("Button: " + add_subsection_btn);
-    //     add_subsection_btn.bind("click", function() {
-    //         add_subsection(add_subsection_btn);
-    //     });
-    // });
-
     $("#sections_content").on("click", ".add_subsection", function(e){
-        return $('<div class="subsection_block secblock" data-section="1.1"> \
-<h4 class="secnum">1.1</h4> \
+        var newsec = $('<div class="subsection_block secblock" data-section="1.1"> \
+<h4 class="subsecnum">1.1</h4> \
 <h4 class="subsectitle edit" contenteditable="true">New Subsection</h4> \
 <div class="subsection_btns"> \
 <button class="btn btn-default remove_subsection" title="Remove Subsection"><span class="glyphicon glyphicon-remove"></span></button> \
@@ -44,13 +35,15 @@ $(document).ready(function() {
 <button class="btn btn-default add_subsection" title="Add Subsection"><span class="glyphicon glyphicon-plus"></span></button> \
 <div> \
 <div>').insertAfter($(this).closest(".secblock"));
+        refresh_indices();
+        newsec.find(".edit").focus();
     });
 
     $("#sections_content").on("click", ".add_section", function(e){
-        return $('<div class="section_row"> \
-            <div class="section_block secblock" data-section="1"> \
+        var newsec = $('<div class="section_row"> \
+<div class="section_block secblock" data-section="1"> \
 <h3 class="secnum">1.</h3> \
-<h3 class="sectitle edit" contenteditable="true">JSP Overview</h3> \
+<h3 class="sectitle edit" contenteditable="true">New Section</h3> \
 <div class="section_btns"> \
 <button class="btn btn-default remove_section" title="Remove Section"><span class="glyphicon glyphicon-remove"></span></button> \
 <button class="btn btn-default edit_section" title="Edit Section"><span class="glyphicon glyphicon-list-alt"></span></button> \
@@ -58,21 +51,39 @@ $(document).ready(function() {
 </div> \
 </div> \
 <button class="btn btn-default btn-block add_section" title="Add Section"><span class="glyphicon glyphicon-plus"></span></button> \
-</div>').insertAfter($(this).closest(".section_row"));
+</div>').insertAfter($(this).closest(".section_row")).focus();
+        refresh_indices();
+        newsec.find(".edit").focus();
     });
 
     $("#sections_content").on("click", ".remove_section", function(e){
-        var r = confirm("Are you absolutely sure you want to remove this section?");
+        var r = confirm("Are you absolutely sure you want to remove this section?\nAll contained pages will be deleted.");
         if (r) {
             $(this).closest(".section_row").remove();
         }
+
+        refresh_indices();
     });
 
     $("#sections_content").on("click", ".remove_subsection", function(e){
-        var r = confirm("Are you sure you want to remove this subsection?");
+        var r = confirm("Are you sure you want to remove this subsection?\nAll contained pages will be deleted.");
         if (r) {
             $(this).closest(".secblock").remove();
         }
+
+        refresh_indices();
     });
+
+    function refresh_indices() {
+        var i = 0;
+        $(".section_row").each(function(i) {
+            var secnum = $(this).find(".secnum");
+            secnum.html(i+1 + ".");
+            $(this).find(".subsecnum").each(function(j) {
+                var subsecnum = $(this);
+                subsecnum.html((i+1) + "." + (j+1));
+            });
+        });
+    }
 
 })
