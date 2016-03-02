47fccba80bd8b706d5e168f1195917ebb5c4d9fe
Chris von Zuben <chris.vonzuben@gmail.com>
2016-03-01 22:30:55 -0500

    Add grid disable to avoid content overlap

--- dev/drag_and_drop/builder.js
+++ dev/drag_and_drop/builder.js
@@ -21,16 +21,65 @@ function set_drag_listener () {
     }
 }
 
+function is_disabled_grid (grid) {
+    // check if the grid item is disabled
+    var disabled = grid.attributes.getNamedItem("grid_disabled");
+    if (disabled) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 function add_file_item_events (el) {
     el.ondragstart = file_item_drag;
 }
 
 function add_drag_style (el) {
-    el.classList.add("drag_hover");
+    if (!is_disabled_grid(el)) {
+        el.classList.add("drag_hover");
+    }
 }
 
 function remove_drag_style (el) {
     el.classList.remove("drag_hover");
+}
+
+function enable_grids(grid) {
+    var id = grid.id;
+    
+    var effected_grids = document.querySelectorAll("." + id);
+    
+    var i = 0;
+    while (i < effected_grids.length) {
+        if (effected_grids[i].attributes.getNamedItem("grid_disabled")) {
+            effected_grids[i].attributes.removeNamedItem("grid_disabled");  
+            effected_grids[i].classList.remove("grid_disabled");
+        }
+        i += 1;
+    }
+}
+
+function disable_grids(grid) {
+    var id = grid.id;
+    
+    var effected_grids = document.querySelectorAll("." + id);
+    
+    var i = 0;
+    while (i < effected_grids.length) {
+        if (!effected_grids[i].attributes.getNamedItem("grid_disabled")) {
+            effected_grids[i].setAttribute("grid_disabled", null);
+            effected_grids[i].classList.add("grid_disabled");
+        }
+        i += 1;
+    }
+}
+
+function add_file_to_grid (grid, file) {
+    //console.log(file);
+    //console.log(grid);
+    disable_grids(grid);
 }
 
 function grid_drag_enter (ev) {
@@ -56,9 +105,17 @@ function file_item_drag(ev) {
 
 function grid_drop(ev) {
     ev.preventDefault();
+    var grid = ev.target;
     var id = ev.dataTransfer.getData("id");
     var file = document.getElementById(id);
-    console.log(file);
-    remove_drag_style(ev.target);
-    //ev.target.appendChild(document.getElementById(data));
+    
+    remove_drag_style(grid);
+
+    if (is_disabled_grid(grid)) {
+        console.log("disabled");
+        return;
+    }
+    else {
+        add_file_to_grid(grid, file);   
+    }    
 }
